3083418cf6139f96c753471382c2a9b16da0c64d
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-02-11 00:57:11 -0800

    servlets

---
+++ src/com/dhiva/myservelts/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.dhiva.myservelts;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.getWriter().println("Hello, world");
+	}
+
+	
+
+}
