0e3bd2570bb15804bd8b67778049ed30d1db9079
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-03-21 23:10:23 -0700

    post handling

--- src/com/dhiva/myservelts/HelloWorld.java
+++ src/com/dhiva/myservelts/HelloWorld.java
@@ -3,8 +3,10 @@ package com.dhiva.myservelts;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final String loginID = "aaa";
+	private final String password = "bbb";
     public HelloWorld() {
         // TODO Auto-generated constructor stub
     }
@@ -29,7 +33,26 @@ public class HelloWorld extends HttpServlet {
 		out.write(s);
 		out.flush();
 	}
-
-	
-
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	String login = request.getParameter("login");
+		String passwd = request.getParameter("passwd");
+		if(loginID.equals(login) && password.equals(passwd)){
+			
+			
+			String s = "Hello,"+login;
+	    	response.setStatus(200);
+			response.setContentType("text/plain");
+			response.setContentLength(s.getBytes().length);
+			Cookie loginCookie = new Cookie("login",login);
+			//setting cookie to expiry in 30 mins
+			loginCookie.setMaxAge(30*60);
+			response.addCookie(loginCookie);
+			PrintWriter out = response.getWriter();
+			out.write(s);
+			out.flush();
+			//response.sendRedirect("LoginSuccess.jsp");
+		}
+	}	
 }
